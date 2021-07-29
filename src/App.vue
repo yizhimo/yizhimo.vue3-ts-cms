856eb89b4b3e10af4692c5ef66d1c54fcd378bc0
一只馍 <yizhimo@yizhimodeMBP.lan>
2021-07-29 21:01:52 +0800

    style(init): yizhimo style

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default defineComponent({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e51;
+  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
