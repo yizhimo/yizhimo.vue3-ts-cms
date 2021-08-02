b549e9caf38affae23d0d3b67898cb1418714f37
一只馍 <yizhimo@yizhimodeMacBook-Pro.local>
2021-08-02 10:39:18 +0800

    style: config
    config

---
+++ src/global/register-element.ts
@@ -0,0 +1,12 @@
+// element-plus 局部注册
+import { App } from 'vue'
+import 'element-plus/lib/theme-chalk/base.css'
+import { ElButton } from 'element-plus'
+
+const components = [ElButton]
+
+export default function (app: App): void {
+  for (const component of components) {
+    app.component(component.name, component)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { createApp } from 'vue'
+import { globalRegister } from './global'
 import App from './App.vue'
 
 import router from './router'
 import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+
+// element-plus 全局注册
+// import ElementPlus from 'element-plus'
+// import 'element-plus/lib/theme-chalk/index.css'
+// app.use(ElementPlus)
 
 const app = createApp(App)
+app.use(globalRegister)
 app.use(router)
 app.use(store)
-app.use(ElementPlus)
+
 app.mount('#app')
